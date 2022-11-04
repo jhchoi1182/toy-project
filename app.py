1b61a947ae4133ed85ebecaddf88b153f4860de8
jihyeon choi <jhchoi1182@gmail.com>
2022-11-04 20:48:57 +0900

    merge test commit

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
 
 
+# 상영예정작 크롤링 ##
+#
 # tagTemp = '#contents > div.wrap-movie-chart > div.sect-movie-chart > ol:nth-child(number) > li'
 # for i in range(4,52,2):
 #     tag = tagTemp.replace('number',str(i))
