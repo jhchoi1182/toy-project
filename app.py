da25cbdb44b9b44baccca2fc0fdee38c239e35b4
JongBeom Park <jb753858@gmail.com>
2022-11-05 18:51:27 +0900

    13 commit

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.lyxqol2.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
 headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('http://www.cgv.co.kr/movies/pre-movies.aspx', headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
-
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/pre-movies')
 def premovies():
     return render_template('premovies.html')
+
 
 @app.route("/movie", methods=["POST"])
 def movie_post():
@@ -109,12 +112,11 @@ def movie_post():
     return jsonify({'msg': '로딩완료'})
 
 
-
 @app.route("/movie", methods=["GET"])
 def movie_get():
     movie_list = list(db.movies.find({}, {'_id': False}))
 
-    return jsonify({'movies':movie_list})
+    return jsonify({'movies': movie_list})
 
 
 @app.route('/pre', methods=['GET'])
@@ -122,5 +124,6 @@ def pre_get():
     premovie_list = list(db.preMovies.find({}, {'_id': False}))
     return jsonify({'preMovies': premovie_list})
 
+
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
